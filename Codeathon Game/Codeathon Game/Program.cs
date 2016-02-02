0db3162609636ec10e23fd3d02cd1f55721086c0
Tom4455 <thomaswiddis@gmail.com>
2016-02-02 18:14:17 +0000

    Renamed Game1 to Game

--- Codeathon Game/Codeathon Game/Program.cs
+++ Codeathon Game/Codeathon Game/Program.cs
@@ -14,7 +14,7 @@ namespace Codeathon_Game
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Game())
                 game.Run();
         }
     }
