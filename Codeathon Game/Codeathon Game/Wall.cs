41cc02efc96cfcf8f0234d12e214128a21547afe
Alwin Hughes <alwin.hughes@btopenworld.com>
2016-02-06 13:08:06 +0000

    created wall class

---
+++ Codeathon Game/Codeathon Game/Wall.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+namespace Codeathon_Game
+{
+    class Wall: ObjectToDraw
+    {
+
+        public enum WallState
+        {
+            STRAIT,CORNER,T_JUNCTION,CROSS
+        } 
+        public enum WallRoation
+        {
+            ZERO = 0,ONE = 1,TWO = 2,THREE = 3,
+        
+        }
+        
+        public Wall(Vector2 location, WallRoation,WallState)
+            :base(location, 64,64)
+        {
+            this.location = location;
+        }
+        public Wall(Vector2 location)
+            :base(location, 64,64)
+        {
+            this.location = location;
+        }
+    }
+}
