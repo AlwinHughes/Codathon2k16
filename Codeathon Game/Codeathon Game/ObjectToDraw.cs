991305f9e177ae1ac5d83314f5bde8f9e4d91465
Alwin Hughes <alwin.hughes@btopenworld.com>
2016-02-06 09:27:38 +0000

    added variables and imports

--- Codeathon Game/Codeathon Game/ObjectToDraw.cs
+++ Codeathon Game/Codeathon Game/ObjectToDraw.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace Codeathon_Game
 {
     class ObjectToDraw
     {
+        public Texture2D texture;
+        public Vector2 location;
+        public float rotation;
+        public int width { get; private set; }
+        public int height { get; private set; }
+        public bool canBeDraged;
+
+        public ObjectToDrawBase dock;
+        public Vector2 dockOffset;
+        public bool is_text_show = false;
+
     }
 }
--- Codeathon Game/Codeathon Game/TextShow.cs
+++ Codeathon Game/Codeathon Game/TextShow.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace Codeathon_Game
 {
