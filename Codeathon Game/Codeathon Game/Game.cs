60710088f973dafbbd30ca0f510dcec2180ac108
Tom4455 <thomaswiddis@gmail.com>
2016-02-06 10:00:42 +0000

    added fonts

--- Codeathon Game/Codeathon Game/Game.cs
+++ Codeathon Game/Codeathon Game/Game.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Codeathon_Game
 {
@@ -11,6 +12,8 @@ namespace Codeathon_Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        public static Dictionary<string,SpriteFont> fonts;        
 
         public Game()
         {
@@ -39,6 +42,12 @@ namespace Codeathon_Game
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            fonts = new Dictionary<string, SpriteFont>();
+
+            fonts.Add("font16", Content.Load<SpriteFont>("fonts/font16"));
+            fonts.Add("font24", Content.Load<SpriteFont>("fonts/font24"));
+            fonts.Add("font32", Content.Load<SpriteFont>("fonts/font32"));
+            fonts.Add("font40", Content.Load<SpriteFont>("fonts/font40"));
 
             // TODO: use this.Content to load your game content here
         }
@@ -75,6 +84,12 @@ namespace Codeathon_Game
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            spriteBatch.Begin();
+            spriteBatch.DrawString(fonts["font16"], "HELLO BOSS 16", new Vector2(50, 50), Color.Black);
+            spriteBatch.DrawString(fonts["font24"], "HELLO BOSS 24", new Vector2(50, 100), Color.Black);
+            spriteBatch.DrawString(fonts["font32"], "HELLO BOSS 32", new Vector2(50, 150), Color.Black);
+            spriteBatch.DrawString(fonts["font40"], "HELLO BOSS 40", new Vector2(50, 200), Color.Black);
+            spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
